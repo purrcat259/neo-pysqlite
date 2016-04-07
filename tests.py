e363adae51ed85efe49c4a7ce4b666d0a35c50c4
Simon AM <simon@agius-muscat.net>
2016-04-07 22:33:18 +0200

    Added more tests

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+from pysqlite import Pysqlite
 import unittest
 import os.path
 
@@ -9,3 +10,18 @@ class TestDBAccessible(unittest.TestCase):
     def db_accessible(self):
         self.assertTrue(os.access('test.db', os.R_OK))
 
+
+class TestDBNotEmpty(unittest.TestCase):
+    def test_db_table_exists(self):
+        db = Pysqlite(database_name='test db', database_file='test.db')
+        data = db.get_db_data('sqlite_sequence')
+        table_names = [field[0] for field in data]
+        self.assertTrue('table_one' in table_names)
+
+    def test_db_not_empty(self):
+        db = Pysqlite(database_name='test db', database_file='test.db')
+        data = db.get_db_data('table_one')
+        self.assertGreater(len(data), 0)
+
+if __name__ == '__main__':
+    unittest.main()
