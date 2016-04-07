3f70bb07d19cda12218353f7c1de5c9c6c7b6130
Simon AM <simon@agius-muscat.net>
2016-04-08 00:18:51 +0200

    Added contents count and failure messages

--- tests.py
+++ tests.py
@@ -2,13 +2,15 @@ from pysqlite import Pysqlite
 import unittest
 import os.path
 
+# TODO: Pass database filename and tables names via argparse to run tests against a specific database
+
 
 class TestDBAccessible(unittest.TestCase):
     def test_db_exists(self):
-        self.assertTrue(os.path.isfile('test.db'))
+        self.assertTrue(os.path.isfile('test.db'), msg='Database file does not exist')
 
     def db_accessible(self):
-        self.assertTrue(os.access('test.db', os.R_OK))
+        self.assertTrue(os.access('test.db', os.R_OK), msg='Database file could not be accessed')
 
 
 class TestDBNotEmpty(unittest.TestCase):
@@ -16,12 +18,20 @@ class TestDBNotEmpty(unittest.TestCase):
         db = Pysqlite(database_name='test db', database_file='test.db')
         data = db.get_db_data('sqlite_sequence')
         table_names = [field[0] for field in data]
-        self.assertTrue('table_one' in table_names)
+        self.assertTrue('table_one' in table_names, msg='Table table_one does not exist')
 
     def test_db_not_empty(self):
         db = Pysqlite(database_name='test db', database_file='test.db')
         data = db.get_db_data('table_one')
-        self.assertGreater(len(data), 0)
+        self.assertGreater(len(data), 0, msg='Test table_one is empty')
+
+
+class TestDBContents(unittest.TestCase):
+    def test_contents_count(self):
+        db = Pysqlite(database_name='test db', database_file='test.db')
+        data = db.get_db_data('table_one')
+        self.assertEqual(len(data), 4, msg='Test contents not as expected')
+
 
 if __name__ == '__main__':
     unittest.main()
