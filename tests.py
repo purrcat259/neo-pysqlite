cc745031a18f2631f7e4b0b34d79a49e562f2fb0
Simon AM <simon@agius-muscat.net>
2016-04-12 15:09:30 +0200

    Added table check test

--- tests.py
+++ tests.py
@@ -7,6 +7,7 @@ import os.path
 # set the DB as global to avoid initialising it each time
 db = Pysqlite(database_name='test db', database_file='test.db')
 
+
 class TestDBAccessible(unittest.TestCase):
     def test_db_exists(self):
         self.assertTrue(os.path.isfile('test.db'), msg='Database file does not exist')
@@ -31,11 +32,20 @@ class TestDBNotEmpty(unittest.TestCase):
 
 
 class TestDBContents(unittest.TestCase):
+    def test_tables_exist(self):
+        global db
+        test_table_names = ['table_one', 'sqlite_sequence']
+        table_names = db.get_table_names()
+        self.assertEqual(table_names, test_table_names, msg='Tables returned: {} not as expected: {}'.format(
+            table_names, test_table_names))
+
+    """
     def test_contents_count(self):
         # db = Pysqlite(database_name='test db', database_file='test.db')
         global db
         data = db.get_db_data('table_one')
         self.assertEqual(len(data), 4, msg='Test contents not as expected')
+    """
 
     def test_insert_correct_row(self):
         global db
