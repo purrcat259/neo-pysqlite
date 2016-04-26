529c27f16fc5dc90a9c7669a45818d58b8ffb5d9
Simon AM <simon@agius-muscat.net>
2016-04-26 19:53:47 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='pysqlite',
+        version='0.2',
+        packages=['pysqlite'],
+        url='https://github.com/purrcat259/pysqlite',
+        download_url='https://github.com/purrcat259/pysqlite/tarball/0.2',
+        license='GNU GPL v3',
+        author='Simon',
+        author_email='simon@agius-muscat.net',
+        description='Python wrapper around SQLite'
+)
